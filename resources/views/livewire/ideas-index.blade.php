850260522b8d6ab3e9e9e2fa6604f32c9144d4dc
Mickey Berg <mickeybrasmussen@gmail.com>
2022-04-07 14:34:37 +0200

    search and tests

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -19,7 +19,7 @@
             </select>
         </div>
         <div class="relative w-full md:w-2/3">
-            <input type="search" placeholder="Find an idea" class="w-full px-4 py-2 pl-8 placeholder-gray-900 bg-white border-none rounded-xl">
+            <input wire:model="search" type="search" placeholder="Find an idea" class="w-full px-4 py-2 pl-8 placeholder-gray-900 bg-white border-none rounded-xl">
             <div class="absolute top-0 flex h-full ml-2 itmes-center">
                 <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -30,13 +30,17 @@
 
     <div class="my-8 space-y-6 ideas-container">
 
-        @foreach($ideas as $idea)
+        @forelse($ideas as $idea)
             <livewire:idea-index
                 :key="$idea->id"
                 :idea="$idea"
                 :votesCount="$idea->votes_count"
-            />
-        @endforeach
+            />   
+        @empty
+            <div class="mx-auto mt-12 w-70">
+                <div class="mt-6 font-bold text-center text-gray-400">Nothing was found... <br> Try searching for something else.</div>
+            </div>
+        @endforelse
 
     </div> <!-- end ideas-container -->
 
