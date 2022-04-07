ac95058ce38004bf02d9881a90eaf7a04ff1d5b5
Mickey Berg <mickeybrasmussen@gmail.com>
2022-04-07 11:41:21 +0200

    statuses

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Idea;
 use App\Models\Vote;
+use App\Models\Status;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -13,15 +14,21 @@ class IdeasIndex extends Component
     
     public function render()
     {
+        // array of status names and ids
+        $statuses = Status::all()->pluck('id', 'name');
+
         return view('livewire.ideas-index', [
             'ideas' => Idea::with('user', 'category', 'status')
-                ->addSelect(['voted_by_user' => Vote::select('id')
-                    ->where('user_id', auth()->id())
-                    ->whereColumn('idea_id', 'ideas.id')
-                ])
-                ->withCount('votes')
-                ->orderBy('id', 'desc')
-                ->simplePaginate(Idea::PAGINATION_COUNT)
+                ->when(request()->status && request()->status !== 'All', function($query) use ($statuses) {
+                    return $query->where('status.id', $statuses->get(request()->status));
+            })
+            ->addSelect(['voted_by_user' => Vote::select('id')
+                ->where('user_id', auth()->id())
+                ->whereColumn('idea_id', 'ideas.id')
+            ])
+            ->withCount('votes')
+            ->orderBy('id', 'desc')
+            ->simplePaginate(Idea::PAGINATION_COUNT)
         ]);
     }
 }
