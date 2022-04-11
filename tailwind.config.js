af257511e933fda2e4d18867121766d76d9a52ff
Mickey Berg <mickeybrasmussen@gmail.com>
2022-04-11 12:10:30 +0200

    set status mail notifier

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,9 @@ module.exports = {
                 xxs: ["0.625rem", { lineHeight: "1rem" }],
             },
         },
+        variants: {
+            opacity: ({ after }) => after(["disabled"]),
+        },
     },
 
     plugins: [
